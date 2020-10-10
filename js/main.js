b662397fd15d1c36f5acfadd8041b5f55be85254
mfaldzinski <61914702+mfaldzinski@users.noreply.github.com>
2020-10-10 14:20:31 +0200

    add .js

---
+++ js/main.js
@@ -0,0 +1,96 @@
+/* 
+----------
+Portfolio
+----------
+*/
+$('.myPortfolios ul').slick({
+    dots: true,
+    infinite: true,
+    arrows: false,
+    speed: 5,
+    autoplay: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    responsive: [{
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
+});
+
+/* 
+-----------
+Burger Menu
+-----------
+*/
+$('.burgerMenu').click(function() {
+    $('#nav ul').slideToggle();
+});
+
+/* 
+--------------------
+Horizontal Scrollbar
+--------------------
+*/
+function scrollIndicator() {
+    var windowScroll = $(window).scrollTop();
+    var height = $(document).height() - $(window).height();
+    var scrolled = (windowScroll / height) * 100;
+    $('#scrollBar').css('width', scrolled + '%');
+}
+
+$(window).scroll(function() {
+    scrollIndicator();
+});
+
+
+
+/* 
+---------
+Go to Top
+---------
+*/
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 200) {
+        $('#gotop').show();
+    } else {
+        $('#gotop').hide();
+    }
+});
+
+$('#gotop').click(function() {
+    $('html, body').animate({
+        scrollTop: 0
+    }, 'slow');
+    return false;
+});
+
+/* 
+-------------
+Smooth Scroll
+-------------
+*/
+$(document).ready(function() {
+    var topOffset = $('#header').outerHeight();
+    $('#nav a[href^="#"]').on('click', function(event) {
+        $('.active').removeClass('active');
+        $(this).addClass('active');
+        var target = $(this.getAttribute('href'));
+        if (target.length) {
+            event.preventDefault();
+            $('html, body').animate({
+                scrollTop: target.offset().top - topOffset
+            }, 1000);
+        }
+    });
+});
---
+++ js/script.js
@@ -0,0 +1,10 @@
+//const counter_el = document.querySelector('.counter');
+//const description_el = document.querySelector('.description');
+
+//let count = 0;
+//setInterval(() => {
+//if (count < 1000) {
+//	count++;
+//	counter_el.innerText = count;
+//	}
+//}, 5);
